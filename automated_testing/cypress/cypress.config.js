3c21af4a0b31e67f8e461489bb9e8a9291397db5
bhavesh.jadav <bhavesh.jadav@ncameo.com>
2024-11-18 19:28:37 +0530

    staging store change URl for all file changed for automated stating
    staging store change URl for all file changed for automated stating

---
+++ automated_testing/cypress/cypress.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://staging-lost-river-press.myshopify.com', // Replace with the correct port
+  },
+};
---
+++ cypress.config.js
@@ -0,0 +1,9 @@
+const { defineConfig } = require("cypress");
+
+	module.exports = defineConfig({
+	  e2e: {
+		specPattern: 'automated_testing/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure it matches your file pattern
+		baseUrl: 'https://staging-lost-river-press.myshopify.com/',
+		supportFile: false, // Disable support file
+	  },
+	});
