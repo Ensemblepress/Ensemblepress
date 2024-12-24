8009f0e4477431b42e8f495f584baa8d21f0e1f5
bhaveshkatkiyajadav <bhavesh.jadav@ncameo.com>
2024-12-25 01:32:04 +0530

    Update cypress.config.js
    file code updated

--- automated_testing/cypress/cypress.config.js
+++ automated_testing/cypress/cypress.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   e2e: {
-    baseUrl: 'https://dev-store-lost-river-press.myshopify.com/', // Replace with the correct port
+    baseUrl: 'https://staging-lost-river-press.myshopify.com/', // Replace with the correct port
   },
 };
