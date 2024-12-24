b57498cacad4118bcd1f46fdcc5ccc3477513107
bhavesh.jadav <bhavesh.jadav@ncameo.com>
2024-12-25 01:04:40 +0530

    add or update comment with coded
    add or update comment with coded

--- automated_testing/cypress/e2e/automated/checkoutFlow.cy.js
+++ automated_testing/cypress/e2e/automated/checkoutFlow.cy.js
@@ -3,6 +3,7 @@ describe('Shopify Full checkout Flow', () => {
     
 	// Visit the homepage
     cy.visit('https://dev-store-lost-river-press.myshopify.com');
+	//cy.visit('https://staging-lost-river-press.myshopify.com');
 	
 	//enter store password
 	//cy.get('.password-link').click();
