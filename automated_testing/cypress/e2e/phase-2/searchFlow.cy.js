d09729b2500bd85c3d4e48e8cf50ca116aaf966e
bhaveshkatkiyajadav <bhavesh.jadav@ncameo.com>
2024-12-25 01:24:53 +0530

    Update searchFlow.cy.js
    file code updated

--- automated_testing/cypress/e2e/phase-2/searchFlow.cy.js
+++ automated_testing/cypress/e2e/phase-2/searchFlow.cy.js
@@ -2,7 +2,7 @@ describe('Shopify Product Search Flow', () => {
   it('searches for a product', () => {
     
 	//visit the homepage
-    cy.visit('https://dev-store-lost-river-press.myshopify.com/');
+    cy.visit('https://staging-lost-river-press.myshopify.com/');
 	
 	//enter store password
 	//cy.get('.password-link').click();
