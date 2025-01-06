3e9414ebf96e8aaa99fcc6c134c644b0ab1b3585
bhaveshkatkiyajadav <bhavesh.jadav@ncameo.com>
2025-01-06 21:51:57 +0530

    Update cypress.config.js
    file path change

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ const { defineConfig } = require("cypress");
 	module.exports = defineConfig({
 	  e2e: {
 		specPattern: 'automated_testing/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Ensure it matches your file pattern
-		baseUrl: 'https://staging-lost-river-press.myshopify.com/',
+		baseUrl: 'https://8b47fd-7d.myshopify.com/',
 		supportFile: false, // Disable support file
 	  },
 	});
